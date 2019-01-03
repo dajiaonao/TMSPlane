ecfdccd5dc9d12378072c750eaadb12ac39e9e90
Dongliang Zhang <dongliang.zhang@cern.ch>
2019-01-03 10:46:56 +0800

    update changes

--- Software/Control/src/tuneTest.py
+++ Software/Control/src/tuneTest.py
@@ -5,10 +5,9 @@ from PyDE import *
 import threading
 from Queue import Queue
 import time
-
-def test1():
-    print("testing")
-
+from TMS1mmX19Tuner import SensorConfig, CommonData
+import socket
+from command import *
 
 class tuner(threading.Thread):
     def __init__(self, idx):
@@ -53,6 +52,15 @@ class Train(threading.Thread):
         self.meas = [None]*nChan
         self.on = True
         self.mask = [0]*nChan
+
+        ### for data taking
+        self.s = None
+        self.cmd = None
+        self.sensor_config = None
+        self.cd = None
+
+
+
     def run(self):
         while self.on:
             cnt = 0
@@ -73,11 +81,38 @@ class Train(threading.Thread):
             if cnt == 0: self.on = False
             if cnt1 == 0: continue
 
-            #### calculation
+            #### update sensor
             for i,p in enumerate(self.pars):
-                if p is not None:
-                    self.meas[i] = sum([(x-i+5)*(x-i+5) for x in p])
-#                     print('--- {0:d}--->{1:g}'.format(i,self.meas[i]))
+                if self.mask[i] == 1:
+                    ### update sensor i
+
+            ### take data
+            time.sleep(2.0)
+            meas = [[] for i in range(self.cd.sigproc.nParamMax)]*nChan
+            for i in range(self.cd.atMeasNavg):
+                # reset data fifo
+                self.cd.dataSocket.sendall(self.cd.cmd.send_pulse(1<<2));
+                time.sleep(0.05)
+                buf = self.cd.cmd.acquire_from_datafifo(self.cd.dataSocket, self.cd.nWords, self.cd.sampleBuf)
+                self.cd.sigproc.demux_fifodata(buf, self.cd.adcData, self.cd.sdmData)
+
+                currMeasP = self.cd.sigproc.measure_pulse(self.cd.adcData)
+
+                for j in range(nChan):
+                    if currMeasP[j][2] < self.cd.atTbounds[0] or currMeasP[j][2] > self.cd.atTbounds[1] or currMeasP[j][3] < 0:
+                        ### set -1
+                    for k in range(len(currMeasP[j])):
+                        meas[j][k].append(currMeasP[j][k])
+
+            for j in range(nChan):
+                currMeasP[j] = [sum(x)/len(x) for x in meas[j]]
+                print("AutoTune: meas after {:d} avgs : {:}".format(self.cd.atMeasNavg, currMeasP[j]))
+                ret = -currMeasP[j][3]/currMeasP[j][1]
+                print("AutoTune: ret : ", ret, currMeasP[j][1], currMeasP[j][3])
+                if ret < self.cd.atBestRet:
+                    self.cd.atBestRet = ret
+                    for i in range(len(self.cd.atBestVolts)):
+                        self.cd.atBestVolts[i] = self.cd.inputVs[i]
 
             ### return
             for i,t in enumerate(self.tx_qs):
@@ -96,9 +131,28 @@ class TestClass:
         self.nAdcCh = nAdcCh
 
     def test_tune(self):
+        dataIpPort = '192.168.2.3:1024'
+        sD = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        sD.connect((dataIpPort[0],int(dataIpPort[1])))
+
+        ctrlIpPort = '192.168.2.3:1025'
+        sC = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        sC.connect((ctrlIpPort[0],int(ctrlIpPort[1])))
+
+        cmd = Cmd()
+        cd = CommonData(cmd, dataSocket=sD, ctrlSocket=sC)
+        cd.aoutBuf = 1 # AOUT buffer select, 0:AOUT1, 1:AOUT2, >1:disable both
+        cd.x2gain = 2 # BufferX2 gain 
+        cd.sdmMode = 0 # SDM working mode, 0:disabled, 1:normal operation, 2:test with signal injection 
+        cd.bufferTest = 0 #
+
+        sc1 = SensorConfig(cd, configFName='config.json')
+
         tr1 = Train(self.nAdcCh)
         tr1.tx_qs = self.rx_qs
         tr1.rx_qs = self.tx_qs
+        tr1.sensor_config = sc1
+        tr1.cd = cd
         tr1.on = True
 
         for i in range(self.nAdcCh):
@@ -108,7 +162,6 @@ class TestClass:
             th1.tx_qs = self.tx_qs
             th1.rx_qs = self.rx_qs            
             th1.start()
-#         time.sleep(2)
         tr1.start()
 
 
