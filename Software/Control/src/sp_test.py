723ba2935086581c3adb45b02d21d8a325fd251f
Dongliang Zhang <dzhang@mail.ccnu.edu.cn>
2019-02-15 18:59:52 -0800

    save changes

--- Software/Control/src/sp_test.py
+++ Software/Control/src/sp_test.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+import sys, os
 from ctypes import *
 from sigproc import SigProc 
 from ROOT import *
@@ -70,7 +70,7 @@ def test2():
     s1 = SigProc(nSamples=16384, nAdcCh=20, nSdmCh=19, adcSdmCycRatio=5)
     data1 = (s1.ANALYSIS_WAVEFORM_BASE_TYPE * (s1.nSamples * s1.nAdcCh))()
     data1 = array('f',[0]*(16384*20))
-    inRoot = 'data/fpgaLin/Feb09b_data_2.root'
+    inRoot = 'data/fpgaLin/Feb09b_data_587.root'
     fout1 = TFile(inRoot,'read')
     tree1 = fout1.Get('tree1')
     tree1.SetBranchAddress('adc',data1)
@@ -83,21 +83,32 @@ def test2():
 #     for x in [500, 50, 150, 2500]: sp1.fltParam.push_back(x)
     for x in [500, 15, 50, 2500]: sp1.fltParam.push_back(x)
 
-    for ievt in range(100):
+    plt.ion()
+    plt.show()
+    fig, ax1 = plt.subplots(1, 1, figsize=(28, 10))
+#     fig.set_size_inches(11,8)
+    ax1.set_xlabel('time index')
+    ax1.set_ylabel('U [V]', color='b')
+    ax1.tick_params('y', colors='b')
+    ax2 = ax1.twinx()
+    ax2.set_ylabel('U [V]', color='r')
+    ax2.tick_params('y', colors='r')
+
+#     for ievt in range(tree1.GetEntries()):
+
+    NMax = tree1.GetEntries()
+    ievt = 0
+    while ievt< NMax:
+
+        print "Event:", ievt
         tree1.GetEntry(ievt)
         sp1.measure_pulse2(data1, ich)
 
         vx = np.array([sp1.scrAry[i] for i in range(sp1.nSamples)])
         vo = data1[ich*sp1.nSamples:(ich+1)*sp1.nSamples]
 
-        fig, ax1 = plt.subplots()
-        ax1.set_xlabel('time index')
-        ax1.set_ylabel('U [V]', color='b')
-        ax1.tick_params('y', colors='b')
-        ax2 = ax1.twinx()
-        ax2.set_ylabel('U [V]', color='r')
-        ax2.tick_params('y', colors='r')
-
+        ax1.clear()
+        ax2.clear()
         ax1.plot(vo, label='Raw', color='b')
         ax2.plot(vx, label='Filtered', color='r')
 
@@ -108,11 +119,35 @@ def test2():
             print s.idx, s.im, s.Q, s.w0, s.w1, s.w2
             x1.append(s.im)
             y1.append(s.Q)
+            plt.axvline(x=s.im, linestyle='--', color='black')
 
-        ax2.scatter(x1,y1, label='Analysis')
+#         if x1: ax2.scatter(x1,y1, label='Analysis')
 
-        fig.legend()
-        plt.show()
+        fig.tight_layout()
+        plt.draw()
+        plt.grid(True)
+        plt.pause(0.001)
+
+
+        while True:
+            x = raw_input("Next:")
+            if x=='q': sys.exit()
+            elif len(x)>0 and x[0] == 's':
+                for name in x.split()[1:]:
+                    dirx = os.path.dirname(name)
+                    if not os.path.exists(dirx): os.makedirs(dirx)
+                    plt.savefig(name)
+                    print "saved figure to", name
+            else:
+                try:
+                    ievt = int(x)
+                except ValueError:
+                    ievt += 1
+                break
+            
+
+#         fig.legend()
+#         plt.show()
 
 if __name__ == '__main__':
     test2()
