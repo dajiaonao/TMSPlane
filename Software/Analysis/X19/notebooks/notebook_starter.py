e801ea34f73585d193f01e1c1ebf62e2adc088b5
Dongliang Zhang <dzhang@mail.ccnu.edu.cn>
2019-03-15 08:26:32 -0700

    save changes

--- Software/Analysis/X19/notebooks/notebook_starter.py
+++ Software/Analysis/X19/notebooks/notebook_starter.py
@@ -1,6 +1,7 @@
+import os
+tmsDir = os.getenv('HOME')+'/work/repos/TMSPlane'
 import sys
-#sys.path.append('/data/repos/TMSPlane2/Software/X11/scripts')
-sys.path.append('/home/dlzhang/work/repos/TMSPlane/Software/Control/src')
+sys.path.append(tmsDir+'/Software/Control/src')
 
 from notebook_test import test, get_spectrum
 from rootHelper import getRDF
@@ -9,7 +10,7 @@ import numpy as np
 from ROOT import gPad, gDirectory, gStyle, TCanvas, TGraph
 gStyle.SetPalette(55)
 
-dir1 = '/home/dlzhang/work/repos/TMSPlane/Software/Control/src/data/fpgaLin/'
+dir1 = tmsDir+'/Software/Control/src/data/fpgaLin/'
 
 plt.rc('figure', figsize=(15, 6))
 
