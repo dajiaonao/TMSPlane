bba81e0fc0ac9fc537ed34887b38af0b48ad5ed4
Dongliang Zhang <dzhang@mail.ccnu.edu.cn>
2019-05-07 00:44:44 -0700

    save changes

--- Software/Analysis/X19/scripts/run_calib.py
+++ Software/Analysis/X19/scripts/run_calib.py
@@ -3,7 +3,142 @@
 
 from ROOT import TChain, gDirectory, TFile, TNtuple, TGraphErrors
 from array import array
+import heapq
 from rootUtil3 import waitRootCmdX
+
+def make_calibration_file_C3():
+    dir1 = '/home/dlzhang/work/repos/TMSPlane/Software/Control/src/data/fpgaLin/'
+
+    ch1 = TChain('reco')
+    ch1.Add(dir1+'tpx01a_Apr22T1a_data_*.root')
+    nCh = 19
+
+
+    runnList = []
+
+    runnList.append((0.02, [(524,530),(573,579)]))
+    runnList.append((0.04, [(530,535),(579,584)]))
+    runnList.append((0.08, [(535,567),(584,590)]))
+    runnList.append((0.12, [(500,502),(590,596)]))
+    runnList.append((0.2,  [(502,508),(596,602)]))
+    runnList.append((0.32, [(508,513),(602,607)]))
+    runnList.append((0.4,  [(513,519),(607,613)]))
+    runnList.append((0.5,  [(519,524),(613,618)]))
+    runnList.append((0.8,  [(567,573),(618,624)]))
+
+    vlist = []
+
+    for t in runnList:
+        vlist.append(('v{0:d}mV'.format(int(t[0]*1000)), t[0], 'w2[19]>200&&(im[{0:d}]-im[19])>900&&(im[{0:d}]-im[19])<950&&('+'||'.join(["(run>{0:d}&&run<{1:d})".format(k[0],k[1]) for k in t[1]])+')'))
+
+#     est = [(0.01306547417143738, 0.003698981238129058), (1.6481950554410698, 0.027287810269756456), (0.17888571187127186, 0.0056988026185020154), (0.013718239445064838, 0.0021795764198094163), (6.0042468076098476e-05, 3.0483422075668022e-05), (0.07421304753733172, 0.003478449583955647), (6.212332006845403e-05, 6.779904949559736e-05), (8.169197199926832e-05, 2.6734329252008895e-05), (0.03676218754716031, 0.0045116035321878575), (-0.15777441519155896, 0.05152861618856887), (0.004927751437446848, 0.00011716873695953227), (0.13513578525452913, 0.021298551091621157), (0.04025721256634158, 0.009371108180017544), (-0.00026704016289889605, 0.00019503258946046624), (0.005499667070990824, 6.366489832001911e-05), (0.09173951624455232, 0.0006343706962709002), (-3.287987417283733e-05, 0.00012376020484164573), (0.017738572346043275, 0.002019979302557003), (0.00010283736764666133, 3.557897609309108e-05)]
+#     est = [(0.021749978934179244, 0.00013591348075592935), (0.08800730871757548, 0.0003513179239750194), (0.1790431751894682, 0.005800068229996891), (0.022607062552135883, 0.00013883708533182448), 0, (0.08213227936435925, 0.00024852014715069323), 0, 0, (0.036709284224434556, 0.0045644795914606745), (0.21108763154872096, 0.2735983294228037), (0.022113121635298348, 0.00029626276052992104), (0.019690773205300823, 7.876236398075285e-05), (0.07760912270008022, 0.0003471481401644965), 0, (0.021365979183177168, 0.00011259826167497963), (0.09173812517505246, 0.0005582059864488797), 0, (0.020984970138757515, 0.001021894805432149), (0.01617748014919807, 0.0005626556946697888)]
+#     est = [(0.21022370123491813, 0.0008741161563643704), (0.19629371426485331, 0.0009121788768669809), (-2.9240373043105834e-05, 0.0001299896098810482), (0.1636330548911822, 0.0009869011104165029), (0.1557281174864831, 0.0004713441812800613), (0.08165197997497343, 0.0002599914611321653), (0.12921528380497121, 0.0006022973770565171), (0.08212465152837047, 0.0008805863461647292), (-0.0009507593531406199, 0.001174828585932144), (0.2245892308053989, 0.0005665017186336387), (0.15689959575915818, 0.0012273326442494626), (0.05964015545309604, 0.00027919815982878804), (0.17694975233278912, 0.0008966022944452189), (0.10315954550972423, 0.001328182705596258), (0.07822323979066328, 0.0017089007391025324), (0.17768504663053067, 0.0006502947947728483), (0.24610372781569553, 0.0012768341750616628), (0.1139013588428892, 0.0003449015240679468), (0.21924261276723744, 0.0005712269654350105)]
+    est = [(0.1,0.04)]*nCh
+
+
+    d1 = [0]*nCh
+
+
+#     isTest = False
+    isTest = True
+    ## let's do the test here
+    if isTest:
+        v = vlist[-1]
+        ich = 18
+        n = ch1.Draw('Q[{0:d}]'.format(ich),v[2].format(ich),'goff')
+        v1 = ch1.GetV1()
+        topN = heapq.nlargest(int(n*0.8), [v1[i] for i in range(n)])
+        print(topN[0], topN[-1])
+
+        print("&&Q[{0:d}]>{1:.3g}&&Q[{0:d}]<{2:.3g}".format(ich, topN[-1],topN[0]))
+        ch1.Draw('Q[{0:d}]>>h1'.format(ich),v[2].format(ich)+"&&Q[{0:d}]>{1:.3g}&&Q[{0:d}]<{2:.3g}".format(ich, topN[-1],topN[0]),'goff')
+        h1 = gDirectory.Get("h1")
+        h1.Fit('gaus')
+        h1.Draw()
+
+        waitRootCmdX()
+
+        fun1 = h1.GetFunction('gaus')
+        m = fun1.GetParameter(1)
+        s = fun1.GetParameter(2)
+        ch1.Draw('Q[{0:d}]>>h2'.format(ich),v[2].format(ich)+"&&Q[{0:d}]>{1:.3g}&&Q[{0:d}]<{2:.3g}".format(ich, m-5*s,m+5*s),'goff')
+        h2 = gDirectory.Get("h2")
+        h2.Fit('gaus')
+        h2.Draw()
+
+        waitRootCmdX()
+
+        return
+
+
+
+
+    fout = TFile('C3_calib_out0.root','recreate')
+    tup1 = TNtuple('calib','calibration info','chan:V:nEvt:mean:meanErr:sigma:sigmaErr:fProb:fStatus')
+
+    for v in vlist:
+        for ich in range(nCh):
+#             gDirectory.DeleteAll('h1*')
+
+#             e = est[ich]
+#             if e ==0: e = (0.1,0.04)
+#             mean = abs(e[0]*v[1])
+#             sigma3 = abs(5.* e[1])
+#             if sigma3 < 0.0001: sigma3 = 0.0001
+#             print(mean, sigma3)
+# 
+            hName = 'h_'+str(ich)+'_'+v[0]
+# #             n = ch1.Draw('Q[{0:d}]>>h1(100,{1:.2g},{2:.2g})'.format(ich,mean-sigma3,mean+sigma3),v[2].format(ich),'goff')
+# #             n = ch1.Draw('Q[{0:d}]>>{1}'.format(ich,hName,mean-sigma3,mean+sigma3),v[2].format(ich)+'&&Q[{0:d}]>{1:.2g}&&Q[{0:d}]<{2:.2g}'.format(ich,0.2*mean,5*mean),'goff')
+#             n = ch1.Draw('Q[{0:d}]>>{1}'.format(ich,hName),v[2].format(ich),'goff')
+#             print(n)
+
+            ### get the signal
+            n0 = ch1.Draw('Q[{0:d}]'.format(ich), v[2].format(ich),'goff')
+            v1 = ch1.GetV1()
+            topN = heapq.nlargest(int(n0*0.8), [v1[i] for i in range(n0)])
+#             print(topN[0], topN[-1])
+
+            n1 = ch1.Draw('Q[{0:d}]>>{1}t'.format(ich, hName),v[2].format(ich)+"&&Q[{0:d}]>{1:.3g}&&Q[{0:d}]<{2:.3g}".format(ich, topN[-1],topN[0]),'goff')
+            h1 = gDirectory.Get(hName+'t')
+            h1.Fit('gaus')
+            fun1 = h1.GetFunction('gaus')
+            m = fun1.GetParameter(1)
+            s = fun1.GetParameter(2)
+            n = ch1.Draw('Q[{0:d}]>>{1}'.format(ich,hName),v[2].format(ich)+"&&Q[{0:d}]>{1:.3g}&&Q[{0:d}]<{2:.3g}".format(ich, m-5*s,m+5*s),'goff')
+
+            if n<1: 
+                tup1.Fill(ich,v[1],n,-1, -1, -1, -1, -1,-1)
+                continue
+            h1 = gDirectory.Get(hName)
+            r = h1.Fit('gaus','S')
+            fun1 = h1.GetFunction('gaus')
+            nC = 7400.*v[1]
+            enc = nC*fun1.GetParameter(2)/fun1.GetParameter(1)
+
+            tup1.Fill(ich,v[1],n,fun1.GetParameter(1), fun1.GetParError(1), fun1.GetParameter(2), fun1.GetParError(2), r.Prob(), r.Status())
+
+            print(enc, r.Prob())
+
+            fout.cd()
+            h1.Write()
+
+#             h1.Draw()
+#             waitRootCmdX()
+
+            d1[ich] = (fun1.GetParameter(1)/v[1],fun1.GetParameter(2))
+    print(d1)
+
+    tup1.Write()
+
+    for ich in range(nCh):
+        gr = get_gr(tup1,ich)
+        gr.Write('calib_gr_'+str(ich))
+
+    fout.Close()
+
+
 
 def make_calibration_file():
     dir1 = '/home/dzhang/work/repos/TMSPlane/Software/Control/src/data/fpgaLin/'
@@ -94,7 +229,8 @@ def get_gr(ch,chan):
     return gr0
 
 def test():
-    make_calibration_file()
+#     make_calibration_file()
+    make_calibration_file_C3()
 
 if __name__ == '__main__':
     test()
